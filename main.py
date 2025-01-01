37b1671839055ec964fdb5253592b220598dcc94
Felix Fennell <felnne@bas.ac.uk>
2025-01-01 10:05:42 +0000

    Refactoring metrics to separate page

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import streamlit as st
 from shared import app_version
 
 find_page = st.Page("page_find.py", title="Find volunteers with skills", icon=":material/manage_search:")
+stats_page = st.Page("page_stats.py", title="Volunteer skills statistics", icon=":material/insights:")
 update_page = st.Page("page_update.py", title="Update volunteer skills", icon=":material/edit:")
-app = st.navigation([find_page, update_page])
+app = st.navigation([find_page, stats_page, update_page])
+
 st.set_page_config(page_title="MapAction Skills", page_icon=":material/point_scan:")
 with st.sidebar:
     st.markdown(
--- page_find.py
+++ page_find.py
@@ -15,24 +15,8 @@ def show_skills_query(data: VolunteerSkillsClient) -> None:
             st.warning("No volunteers found with all the selected skills.")
 
 
-def show_skills_stats(data: VolunteerSkillsClient) -> None:
-    st.header("Volunteer skills statistics", divider=True)
-
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Volunteers", data.count_volunteers)
-    col2.metric("Skills (Possible)", data.count_skills_possible)
-    col3.metric("Skills (Available)", data.count_skills_available)
-
-    st.info("These metrics and charts were me messing around with streamlit, I don't think they're very useful.")
-
-    tab1, tab2 = st.tabs(["Volunteer skills", "Skill count"])
-    tab1.bar_chart(data.chart_volunteers_skills, horizontal=True)
-    tab2.bar_chart(data.chart_skills, horizontal=True)
-
-
 conn: SQLConnection = st.connection("neon", type="sql")
 engine = VolunteerSkillsClient(conn=conn)
 
 show_intro()
 show_skills_query(data=engine)
-show_skills_stats(data=engine)
